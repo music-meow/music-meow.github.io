f44686ef949fbcc5330680348fa3c317dc8c454d
music-meow <normmorrison@NormanMisonsMBP.home>
2016-08-22 20:27:59 -0400

    mission/js/index.js

--- mission/js/index.js
+++ mission/js/index.js
@@ -37,7 +37,7 @@ function Hide() {
 }
 
 function Up() {
-   story.innerHTML = "You go to the top, you close the door, and are ambushed by the army. That is the end of your life, Agent " +name+ ".";
+   story.innerHTML = "You go to the top, you close the door, and are ambushed by the army. That is the end of your life, Agent " +name ".";
   opt1.innerHTML = "Try Again";
   opt1.onclick = function() {restart()};
   opt2.innerHTML = "";
